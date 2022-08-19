a0bfc631a70ebd612ecc1d563d3cd8ea71e38808
shwatanap <watanabeshou1520@gmail.com>
2022-08-19 22:39:10 +0900

    :sparkles: serverのtemplate追加

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module github.com/mahiro72/SPAJAM2022/server
+
+go 1.18
+
+require (
+	github.com/google/wire v0.5.0
+	github.com/lib/pq v1.10.6
+)
---
+++ server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/mahiro72/SPAJAM2022/server/infra/sql"
+	"github.com/mahiro72/SPAJAM2022/server/presen/handler"
+	"github.com/mahiro72/SPAJAM2022/server/wire"
+)
+
+func main() {
+	db := sql.NewDriver()
+	userHandler := wire.InitTemplateHandler(db)
+
+	handler.InitRouting(userHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	srv := &http.Server{
+		Addr: ":" + port,
+	}
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalln("Server closed with error:", err)
+	}
+}
