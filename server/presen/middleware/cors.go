cc524c2b4042d223272feff9919b7b4c1590a597
shwatanap <watanabeshou1520@gmail.com>
2022-08-21 07:32:31 +0900

    next.ServeHTTpしてない問題

--- server/presen/middleware/cors.go
+++ server/presen/middleware/cors.go
@@ -4,10 +4,11 @@ import "net/http"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,UPDATE,OPTIONS")
 		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
 	})
 }
